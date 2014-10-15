6656210cff307b5bd49ca1b26982391892f81736
estebanwasinger <estebanwasinger@gmail.com>
2014-10-15 13:30:25 -0300

    - Se saco la edad de los Jugadores, ya que es un elemento calculable - Se agrego el procedure darDeBaja.sql

---
+++ futbol5-3052-03-arena/src/resources/darDeBaja.sql
@@ -0,0 +1,5 @@
+DELIMITER //
+CREATE procedure DarDeBaja(P_Id_Jugador int, P_Id_Partido int)
+BEGIN
+DELETE FROM nombrependiente.Inscripciones WHERE Id_Jugador = P_Id_Jugador and Id_Partido = P_Id_Partido;
+END//
