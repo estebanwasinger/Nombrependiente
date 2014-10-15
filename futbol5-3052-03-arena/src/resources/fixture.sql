acdf4c750a3aac7c998372861552c914bf6924cb
estebanwasinger <estebanwasinger@gmail.com>
2014-10-14 23:52:14 -0300

    Se arreglaron los nombres

--- futbol5-3052-03-arena/src/resources/fixture.sql
+++ futbol5-3052-03-arena/src/resources/fixture.sql
@@ -1,12 +1,14 @@
-INSERT INTO tiposinscripcion
+use nombrependiente;
+
+INSERT INTO TiposInscripcion
 (PRIORIDAD, DESCRIPCION)
 VALUES (1, 'Estandar');
 
-INSERT INTO tiposinscripcion
+INSERT INTO TiposInscripcion
 (PRIORIDAD, DESCRIPCION)
 VALUES (2, 'Solidario');
 
-INSERT INTO tiposinscripcion
+INSERT INTO TiposInscripcion
 (PRIORIDAD, DESCRIPCION)
 VALUES (3, 'Condicional');
 
@@ -14,45 +16,45 @@ VALUES (3, 'Condicional');
 
 SELECT ID_TIPO_INSCRIPCION
   INTO @Tipoinscripcion1
-  FROM tiposinscripcion
+  FROM TiposInscripcion
  WHERE PRIORIDAD = 1
  LIMIT 1;
 
 SELECT ID_TIPO_INSCRIPCION
   INTO @Tipoinscripcion2
-  FROM tiposinscripcion
+  FROM TiposInscripcion
  WHERE PRIORIDAD = 2
  LIMIT 1;
 
  SELECT ID_TIPO_INSCRIPCION
   INTO @Tipoinscripcion3
-  FROM tiposinscripcion
+  FROM TiposInscripcion
  WHERE PRIORIDAD = 3
  LIMIT 1;
 
 
 
-INSERT INTO jugadores
+INSERT INTO Jugadores
 (NOMBRE, APODO, HANDICAP, EDAD, FECHA_NACIMIENTO, ID_TIPO_INSCRIPCION_DEFAULT)
 VALUES ('Paula', 'Pau', 3, 24, '1990-08-27', @Tipoinscripcion1);
 
-INSERT INTO jugadores
+INSERT INTO Jugadores
 (NOMBRE, APODO, HANDICAP, EDAD, FECHA_NACIMIENTO, ID_TIPO_INSCRIPCION_DEFAULT)
 VALUES ('Esteban', 'Estebanquito', 4, 22, '1992-08-27', @Tipoinscripcion2);
 
-INSERT INTO jugadores
+INSERT INTO Jugadores
 (NOMBRE, APODO, HANDICAP, EDAD, FECHA_NACIMIENTO, ID_TIPO_INSCRIPCION_DEFAULT)
 VALUES ('Carolina', 'Caro', 7, 26, '1998-08-27', @Tipoinscripcion2);
 
-INSERT INTO jugadores
+INSERT INTO Jugadores
 (NOMBRE, APODO, HANDICAP, EDAD, FECHA_NACIMIENTO, ID_TIPO_INSCRIPCION_DEFAULT)
 VALUES ('Fernando', 'Fer', 8, 28, '1986-01-01', @Tipoinscripcion3);
 
-INSERT INTO jugadores
+INSERT INTO Jugadores
 (NOMBRE, APODO, HANDICAP, EDAD, FECHA_NACIMIENTO, ID_TIPO_INSCRIPCION_DEFAULT)
 VALUES ('Amigo1', 'Amiguito1', 1, 30, '1984-06-12', @Tipoinscripcion2);
 
-INSERT INTO jugadores
+INSERT INTO Jugadores
 (NOMBRE, APODO, HANDICAP, EDAD, FECHA_NACIMIENTO, ID_TIPO_INSCRIPCION_DEFAULT)
 VALUES ('Amigo2', 'Amiguito2', 6, 32, '1982-08-27', @Tipoinscripcion3);
 
@@ -60,47 +62,47 @@ VALUES ('Amigo2', 'Amiguito2', 6, 32, '1982-08-27', @Tipoinscripcion3);
 
  SELECT ID_JUGADOR
   INTO @Jugador1
-  FROM jugadores
+  FROM Jugadores
  WHERE nombre = 'Paula'
  LIMIT 1;
 
  SELECT ID_JUGADOR
   INTO @Amigo1
-  FROM jugadores
+  FROM Jugadores
  WHERE nombre = 'Carolina'
  LIMIT 1;
 
   SELECT ID_JUGADOR
   INTO @Amigo2
-  FROM jugadores
+  FROM Jugadores
  WHERE nombre = 'Esteban'
  LIMIT 1;
 
 
 
-INSERT INTO amigos
+INSERT INTO Amigos
 (ID_JUGADOR, ID_AMIGO)
 VALUES(@Jugador1, @Amigo1);
 
-INSERT INTO amigos
+INSERT INTO Amigos
 (ID_JUGADOR, ID_AMIGO)
 VALUES(@Jugador1, @Amigo2);
 
 
 
-INSERT INTO partidos
+INSERT INTO Partidos
 (LOCALIDAD, CONFIRMADO)
 VALUES('Berazategui', TRUE);
 
-INSERT INTO partidos
+INSERT INTO Partidos
 (LOCALIDAD, CONFIRMADO)
 VALUES('Capital Federal', TRUE);
 
-INSERT INTO partidos
+INSERT INTO Partidos
 (LOCALIDAD, CONFIRMADO)
 VALUES('San Martin', FALSE);
 
-INSERT INTO partidos
+INSERT INTO Partidos
 (LOCALIDAD, CONFIRMADO)
 VALUES('Zarate', FALSE);
 
@@ -108,25 +110,25 @@ VALUES('Zarate', FALSE);
 
   SELECT ID_PARTIDO
   INTO @Partido1
-  FROM partidos
+  FROM Partidos
  WHERE LOCALIDAD = 'Berazategui'
  LIMIT 1;
 
    SELECT ID_PARTIDO
   INTO @Partido2
-  FROM partidos
+  FROM Partidos
  WHERE LOCALIDAD = 'Capital Federal'
  LIMIT 1;
 
    SELECT ID_PARTIDO
   INTO @Partido3
-  FROM partidos
+  FROM Partidos
  WHERE LOCALIDAD = 'San Martin'
  LIMIT 1;
 
    SELECT ID_PARTIDO
   INTO @Partido4
-  FROM partidos
+  FROM Partidos
  WHERE LOCALIDAD = 'Zarate'
  LIMIT 1;
 
@@ -134,90 +136,90 @@ VALUES('Zarate', FALSE);
 
     SELECT ID_TIPO_INSCRIPCION_DEFAULT
   INTO @Inscripjugador
-  FROM jugadores
+  FROM Jugadores
  WHERE ID_JUGADOR = @Jugador1
  LIMIT 1;
 
     SELECT ID_TIPO_INSCRIPCION_DEFAULT
   INTO @Inscripamigo1
-  FROM jugadores
+  FROM Jugadores
  WHERE ID_JUGADOR = @Amigo1
  LIMIT 1;
 
      SELECT ID_TIPO_INSCRIPCION_DEFAULT
   INTO @Inscripamigo2
-  FROM jugadores
+  FROM Jugadores
  WHERE ID_JUGADOR = @Amigo1
  LIMIT 1;
 
 
 
-INSERT INTO inscripciones
+INSERT INTO Inscripciones
 (ID_JUGADOR, ID_PARTIDO, ID_TIPO_INSCRIPCION)
 VALUES(@Jugador1, @Partido1, @Inscripjugador);
 
-INSERT INTO inscripciones
+INSERT INTO Inscripciones
 (ID_JUGADOR, ID_PARTIDO, ID_TIPO_INSCRIPCION)
 VALUES(@Jugador1, @Partido2, @Inscripjugador);
 
-INSERT INTO inscripciones
+INSERT INTO Inscripciones
 (ID_JUGADOR, ID_PARTIDO, ID_TIPO_INSCRIPCION)
 VALUES(@Jugador1, @Partido4, @Inscripjugador);
 
-INSERT INTO inscripciones
+INSERT INTO Inscripciones
 (ID_JUGADOR, ID_PARTIDO, ID_TIPO_INSCRIPCION)
 VALUES(@Amigo1, @Partido1, @Inscripamigo1);
 
-INSERT INTO inscripciones
+INSERT INTO Inscripciones
 (ID_JUGADOR, ID_PARTIDO, ID_TIPO_INSCRIPCION)
 VALUES(@Amigo1, @Partido2, @Inscripamigo1);
 
-INSERT INTO inscripciones
+INSERT INTO Inscripciones
 (ID_JUGADOR, ID_PARTIDO, ID_TIPO_INSCRIPCION)
 VALUES(@Amigo1, @Partido3, @Inscripamigo1);
 
-INSERT INTO inscripciones
+INSERT INTO Inscripciones
 (ID_JUGADOR, ID_PARTIDO, ID_TIPO_INSCRIPCION)
 VALUES(@Amigo2, @Partido3,  @Inscripamigo2);
 
-INSERT INTO inscripciones
+INSERT INTO Inscripciones
 (ID_JUGADOR, ID_PARTIDO, ID_TIPO_INSCRIPCION)
 VALUES(@Amigo2, @Partido1,  @Inscripamigo2);
 
 
 
-INSERT INTO bajas
+INSERT INTO Bajas
 (ID_JUGADOR, ID_PARTIDO, ID_REEMPLAZO)
 VALUES(@Jugador1, @Partido2,  @Amigo2);
 
-INSERT INTO bajas
+INSERT INTO Bajas
 (ID_JUGADOR, ID_PARTIDO)
 VALUES(@Jugador1, @Partido4);
 
 
 
-INSERT INTO infracciones
+INSERT INTO Infracciones
 (ID_JUGADOR, FECHA, MOTIVO)
-VALUES(@Jugador1, 12/06/2014,'Pateo a un compañero');
+VALUES(@Jugador1, '2014-08-03','Pateo a un compañero');
 
-INSERT INTO infracciones
+INSERT INTO Infracciones
 (ID_JUGADOR, FECHA, MOTIVO)
-VALUES(@Jugador1, 11/08/2014,'Falto al partido');
+VALUES(@Jugador1, '2014-08-02','Falto al partido');
 
-INSERT INTO infracciones
+INSERT INTO Infracciones
 (ID_JUGADOR, FECHA, MOTIVO)
-VALUES(@Jugador1, 04/01/2014,'Ingreso a la cancha sin remera');
+VALUES(@Jugador1, '2014-08-01','Ingreso a la cancha sin remera');
 
-INSERT INTO infracciones
+INSERT INTO Infracciones
 (ID_JUGADOR, FECHA, MOTIVO)
-VALUES(@Amigo1, 10/06/2014,'Insulto al arbitro');
+VALUES(@Amigo1, '2014-08-02','Insulto al arbitro');
 
 
 
-INSERT INTO calificaciones
+INSERT INTO Calificaciones
 (ID_JUGADOR,ID_PARTIDO, NOTA, CRITICA)
 VALUES(@Jugador1, @Partido1, 3, 'Jugo muy flojito');
 
-INSERT INTO calificaciones
+INSERT INTO Calificaciones
 (ID_JUGADOR,ID_PARTIDO, NOTA, CRITICA)
 VALUES(@Amigo1, @Partido2, 10, 'Excelentisimo');
--- futbol5-3052-03-arena/src/resources/modelo de datos.sql
+++ futbol5-3052-03-arena/src/resources/modelo de datos.sql
@@ -1,3 +1,5 @@
+use nombrependiente;
+
 CREATE TABLE Jugadores (
 	Id_Jugador int AUTO_INCREMENT NOT NULL PRIMARY KEY,
     Nombre varchar (20) NOT NULL,
@@ -23,7 +25,7 @@ CREATE TABLE Inscripciones (
 CREATE TABLE Bajas (
 	Id_Jugador int NOT NULL,
     Id_Partido int NOT NULL,
-    Id_Reemplazo int 
+    Id_Reemplazo int
 ) ;
 
 CREATE TABLE Calificaciones (
@@ -58,96 +60,96 @@ CREATE TABLE TiposInscripcion (
 /****************************************************************/
 /****************************************************************/
 
-ALTER TABLE Inscripciones ADD 
-	CONSTRAINT PK_JugadorPartido PRIMARY KEY 
+ALTER TABLE Inscripciones ADD
+	CONSTRAINT PK_JugadorPartido PRIMARY KEY
 	(	Id_Jugador,
 		Id_Partido
-	) ; 
- 
- ALTER TABLE Inscripciones ADD 
-	CONSTRAINT FK_Inscripcion_Jugador FOREIGN KEY 
+	) ;
+
+ ALTER TABLE Inscripciones ADD
+	CONSTRAINT FK_Inscripcion_Jugador FOREIGN KEY
 	(
 		Id_Jugador
 	) REFERENCES Jugadores (
 		Id_Jugador
 	), ADD
-    CONSTRAINT FK_Inscripcion_Tipo_Inscripcion FOREIGN KEY 
+    CONSTRAINT FK_Inscripcion_Tipo_Inscripcion FOREIGN KEY
 	(
 		Id_Tipo_Inscripcion
 	) REFERENCES TiposInscripcion (
 		Id_Tipo_Inscripcion
 	), ADD
-	CONSTRAINT FK_Inscripcion_Partido FOREIGN KEY 
+	CONSTRAINT FK_Inscripcion_Partido FOREIGN KEY
 	(
 		Id_Partido
 	) REFERENCES Partidos (
 		Id_Partido
 	);
-    
-ALTER TABLE Bajas ADD 
-	CONSTRAINT PK_Jugador_Partido PRIMARY KEY 
-	(	Id_Jugador,
-		Id_Partido
-	) ; 
- 
- ALTER TABLE Bajas ADD 
-	CONSTRAINT FK_Baja_Jugador FOREIGN KEY 
-	(
-		Id_Jugador
-	) REFERENCES Jugadores (
-		Id_Jugador
-	), ADD
-	CONSTRAINT FK_Baja_Partido FOREIGN KEY 
-	(
-		Id_Partido
-	) REFERENCES Partidos (
-		Id_Partido
-	);  
-    
-ALTER TABLE Amigos ADD 
-	CONSTRAINT PK_Jugador_Amigos PRIMARY KEY 
-	(	Id_Jugador,
-		Id_Amigo
-	) ; 
 
-ALTER TABLE Amigos ADD 
-	CONSTRAINT FK_Amigos_Jugador FOREIGN KEY 
+ALTER TABLE Bajas ADD
+	CONSTRAINT PK_Jugador_Partido PRIMARY KEY
+	(	Id_Jugador,
+		Id_Partido
+	) ;
+
+ ALTER TABLE Bajas ADD
+	CONSTRAINT FK_Baja_Jugador FOREIGN KEY
 	(
 		Id_Jugador
 	) REFERENCES Jugadores (
 		Id_Jugador
 	), ADD
-	CONSTRAINT FK_Amigos_Amigo FOREIGN KEY 
-	(
-		Id_Amigo
-	) REFERENCES Jugadores (
-		Id_Jugador
-	);  
-    
- ALTER TABLE Infracciones ADD 
-	CONSTRAINT FK_Jugador_Inscripcion FOREIGN KEY 
-	(
-		Id_Jugador
-	) REFERENCES Jugadores (
-		Id_Jugador
-	);
-    
- ALTER TABLE Calificaciones ADD 
-	CONSTRAINT FK_Calificacion_Jugador FOREIGN KEY 
-	(
-		Id_Jugador
-	) REFERENCES Jugadores (
-		Id_Jugador
-	), ADD
-	CONSTRAINT FK_Calificacion_Partido FOREIGN KEY 
+	CONSTRAINT FK_Baja_Partido FOREIGN KEY
 	(
 		Id_Partido
 	) REFERENCES Partidos (
 		Id_Partido
 	);
-    
-ALTER TABLE Jugadores ADD 
-	CONSTRAINT FK_TipoInscripcion_Jugador FOREIGN KEY 
+
+ALTER TABLE Amigos ADD
+	CONSTRAINT PK_Jugador_Amigos PRIMARY KEY
+	(	Id_Jugador,
+		Id_Amigo
+	) ;
+
+ALTER TABLE Amigos ADD
+	CONSTRAINT FK_Amigos_Jugador FOREIGN KEY
+	(
+		Id_Jugador
+	) REFERENCES Jugadores (
+		Id_Jugador
+	), ADD
+	CONSTRAINT FK_Amigos_Amigo FOREIGN KEY
+	(
+		Id_Amigo
+	) REFERENCES Jugadores (
+		Id_Jugador
+	);
+
+ ALTER TABLE Infracciones ADD
+	CONSTRAINT FK_Jugador_Inscripcion FOREIGN KEY
+	(
+		Id_Jugador
+	) REFERENCES Jugadores (
+		Id_Jugador
+	);
+
+ ALTER TABLE Calificaciones ADD
+	CONSTRAINT FK_Calificacion_Jugador FOREIGN KEY
+	(
+		Id_Jugador
+	) REFERENCES Jugadores (
+		Id_Jugador
+	), ADD
+	CONSTRAINT FK_Calificacion_Partido FOREIGN KEY
+	(
+		Id_Partido
+	) REFERENCES Partidos (
+		Id_Partido
+	);
+
+ALTER TABLE Jugadores ADD
+	CONSTRAINT FK_TipoInscripcion_Jugador FOREIGN KEY
 	(
 		Id_Tipo_Inscripcion_Default
 	) REFERENCES TiposInscripcion (
